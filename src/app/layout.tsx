d9ff6c52de524d0b2339c08346c5fab3c0b5370b
Macaulay Omoikhoje De-great <degreat.one2020@gmail.com>
2024-11-07 10:41:35 +0100

    openGraph amendment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export const metadata: Metadata = {
   openGraph: {
     images: [
       {
-        url: ".../Countrypedia-preview.png",
+        url: "https://countrypedia-macchristos-projects.vercel.app//Countrypedia-preview.png",
         width: 1200,
         height: 630,
         alt: "This is Countrypedia",
